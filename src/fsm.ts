e2c8481aa93835e33234228aefcf3fcf9b983a2a
Serkal <kali.hinder@ultixtechnology.com>
2026-03-30 17:12:00 +0100

    added transition map with state and event types

--- src/fsm.ts
+++ src/fsm.ts
@@ -5,14 +5,15 @@ type ParcelState =
   | "dispatched"
   | "out_for_delivery"
   | "delivered"
-  | "failed"
-  | "returned";
+  | "failed" //terminal state
+  | "returned"; //terminal state
 
 // A type for all possible transitions/events (think about what triggers a state change, e.g. dispatch, deliver, fail etc.)
 type ParcelEvent =
   | "request"
   | "start"
   | "dispatch"
+  | "delivering"
   | "deliver"
   | "fail"
   | "return";
@@ -24,3 +25,17 @@ interface Parcel {
   createdAt: Date;
   lastUpdatedAt: Date;
 }
+
+type StateEventMapping = {
+  [key in ParcelState]?: {
+    [event in ParcelEvent]?: ParcelState;
+  };
+};
+
+const transitionMap: StateEventMapping = {
+  pending: { start: "processing", fail: "failed" },
+  processing: { dispatch: "dispatched", fail: "failed" },
+  dispatched: { delivering: "out_for_delivery", fail: "failed" },
+  out_for_delivery: { deliver: "delivered", fail: "failed" },
+  delivered: { return: "returned", fail: "failed" },
+};
